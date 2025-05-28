9049827e4861b68c85e02104c3fd435a0fc9e86c
Ramesh Adhikari <ourworld898@gmail.com>
2025-05-28 16:25:30 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express()
+
+// const app = require("express")()
+app.set("view engine", "ejs") // tells express js to set environment for ejs to run  
+
+app.get("/", function (req, res) {
+    // let name = "Manish"
+    res.render("home", { age: 23, name: "Manish" })
+})
+
+app.get("/about", function (req, res) {
+    res.render("about", { text: "about" })
+})
+
+// task 
+// /about - about.ejs(This is about page) vanerw aaunu paryo but , about vanerw lekeko word chai app.js bata pass hunu paryo 
+
+app.listen(4000, function () {
+    console.log("Backend has started at port 4000")
+})
+
+
+
+
---
+++ database/db.js
@@ -0,0 +1,19 @@
+// database connection ko code lekxam 
+
+const { Sequelize } = require("sequelize")
+// const Sequelize = require("sequelize").Sequelize
+
+const sequelize = new Sequelize() // making object from Sequelize class
+
+// Dog dog = new Dog()
+
+
+// const info = {
+//     name : "manish",
+//     age : 23
+// }
+
+// const name = info.name
+// const age = info.age
+// // destructure
+// const {name,age} = info
