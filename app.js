0212c1d5e053999d8a7449dbaed7be7e09819142
Ramesh Adhikari <ourworld898@gmail.com>
2025-05-30 15:54:37 +0545

    test

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-
+require("./database/db.js")
 // const app = require("express")()
 app.set("view engine", "ejs") // tells express js to set environment for ejs to run  
 
--- database/db.js
+++ database/db.js
@@ -2,9 +2,27 @@
 
 const { Sequelize } = require("sequelize")
 // const Sequelize = require("sequelize").Sequelize
+require("dotenv").config() // yo garepaxi only hami dotenv ko data haru yo file ma access garna milxa
 
-const sequelize = new Sequelize() // making object from Sequelize class
+const sequelize = new Sequelize(
+    {
+        database: process.env.database_name,
+        username: process.env.database_username,
+        port: process.env.database_port,
+        host: process.env.database_host,
+        dialect: "mysql",
+        password: process.env.database_password
+    }
+) // making object from Sequelize class
 
+
+sequelize.authenticate()
+    .then(() => {
+        console.log("connected")
+    })
+    .catch((err) => {
+        console.log("error", err);
+    })
 // Dog dog = new Dog()
 
 
@@ -17,3 +35,5 @@ const sequelize = new Sequelize() // making object from Sequelize class
 // const age = info.age
 // // destructure
 // const {name,age} = info
+
+module.exports = sequelize
